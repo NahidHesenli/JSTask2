efa5facbf3cb4c2d199daa589b325bfc430b1a32
Nahid Hesenli <nahid93591646@gmail.com>
2026-05-16 00:57:26 +0400

    Change password prompt message to Azerbaijani

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let login = prompt("Login daxil edin:");
 let password;
-login === "admin" && (password = prompt("Password daxil edin"));
+login === "admin" && (password = prompt("Şifrə daxil edin"));
 login === "admin" && password === "12345" && alert("Admin panelə giriş edildi");
 login === "admin" && password !== "12345" && alert("Şifrə yanlışdır");
 login !== "admin" && alert("Belə istifadəçi yoxdur");
