36f479f65231a0681bc42b69095b736f0e6c2cf2
Nahid <nahid93591646@gmail.com>
2026-05-16 00:56:32 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let login = prompt("Login daxil edin:");
+let password;
+login === "admin" && (password = prompt("Password daxil edin"));
+login === "admin" && password === "12345" && alert("Admin panelə giriş edildi");
+login === "admin" && password !== "12345" && alert("Şifrə yanlışdır");
+login !== "admin" && alert("Belə istifadəçi yoxdur");
